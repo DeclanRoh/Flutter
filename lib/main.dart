9ab55f6693e79ed5289e6ec7d8e6fdcedc137147
gyeonghwan.roh <hwan0970@knu.ac.kr>
2024-01-24 16:45:07 +0900

    margin, padding

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar( title: Text('앱임'), backgroundColor: Colors.blue ),
-          body: Text('안녕'),
+          appBar: AppBar( title: Text('앱임') ),
+          body: Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              width: double.infinity, height: 50, color: Colors.purple,
+              padding: EdgeInsets.fromLTRB(0, 30, 0, 0),
+              // decoration: BoxDecoration(
+              //   border: Border.all(color: Colors.black)
+              // ),
+            ),
+          ),
           bottomNavigationBar: BottomAppBar(
             child: SizedBox(
               height: 70,
