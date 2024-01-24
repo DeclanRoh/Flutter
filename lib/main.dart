62d6b95fe9b37a33560e780c87b6a17b47b648a1
gyeonghwan.roh <hwan0970@knu.ac.kr>
2024-01-24 14:48:02 +0900

    Prettier

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar( title: Text('앱제목'), foregroundColor: Colors.white, backgroundColor: Colors.blue, ),
+          appBar: AppBar(
+            title: Text('앱제목'),
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue,
+          ),
           body: Text('안녕'),
           bottomNavigationBar: BottomAppBar(
             child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Icon(Icons.phone),
-                  Icon(Icons.message),
-                  Icon(Icons.contact_page),
-                ]
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page),
+              ]
             ),
           ),
         )
