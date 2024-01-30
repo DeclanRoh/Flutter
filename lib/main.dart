104ce1627cf870ff739ca7b4a8a2ff5a1f95fa72
gyeonghwan.roh <hwan0970@knu.ac.kr>
2024-01-30 13:17:48 +0900

    StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -11,33 +11,64 @@ void main() {
 * 4. 마무리 디자인
 * */
 
-var a = SizedBox(
-  child: Text('안녕'),
-);
-
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  var a = 1;
+class Test extends StatefulWidget {
+  const Test({super.key});
 
   @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
   Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var name = ['노경환', '전믿음', '전코몽'];
+  var like = [0, 0, 0];
+
+  // state는 값이 변하면 재랜더링이 된다.
+  // state의 경우, 자주 변경할 것만 선언해서 사용한다. 변동되지 않으면 굳이 state에 담을 필요 없다
+  @override
+  Widget build(BuildContext context) {
+
     return MaterialApp(
         home: Scaffold(
           floatingActionButton: FloatingActionButton(
-            child: Text(a.toString()),
+            child: Text('테스트'),
             onPressed: (){
-              a++;
+              setState(() {
+                'test';
+              });
             },
           ),
           appBar: AppBar(
             automaticallyImplyLeading: false,
-            title: const Text('숙제임')
+            title: Text('공부중'),
           ),
           body: ListView.builder(
             itemCount: 3,
             itemBuilder: (c, i){
-              return ContactItem();
+              return ListTile(
+                leading: Text(like[i].toString()),
+                title: Text(name[i]),
+                trailing: FloatingActionButton(
+                  child: Text('좋아요'),
+                  onPressed: (){
+                    setState(() {
+                      like[i] += 1;
+                    });
+                  },
+                ),
+              );
             },
           ),
           bottomNavigationBar: CustomBottomAppBar(),
